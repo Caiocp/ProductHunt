d7683d1132438baf55085b60935d4b66865cea47
Caio Carvalho <ccarvalho451@gmail.com>
2020-02-17 13:22:59 -0300

    Status bar style

---
+++ mobile/src/config/StatusBarConfig.js
@@ -0,0 +1,5 @@
+import { StatusBar } from "react-native";
+
+StatusBar.setBackgroundColor("#DA552F");
+StatusBar.setTranslucent(true);
+StatusBar.setBarStyle("light-content");
--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Routes from "./routes";
+import "./config/StatusBarConfig";
 
 export default function App() {
   return <Routes />;

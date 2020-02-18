afd803bcdf0b51c5cf49b838869fd3c9512e3e60
Caio Carvalho <ccarvalho451@gmail.com>
2020-02-18 14:07:25 -0300

    add status bar style

--- mobile/src/config/StatusBarConfig.js
+++
@@ -1,5 +0,0 @@
-import { StatusBar } from "react-native";
-
-StatusBar.setBackgroundColor("#DA552F");
-StatusBar.setTranslucent(true);
-StatusBar.setBarStyle("light-content");
--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 import Routes from "./routes";
-import "./config/StatusBarConfig";
 
 export default function App() {
   return <Routes />;
